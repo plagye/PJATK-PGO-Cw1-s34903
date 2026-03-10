6bbb4de4a22ddde420c9b57377e762c748fe7bdb
plagye <spotifysess@gmail.com>
2026-03-10 20:29:02 +0100

    Naprawa literówki systenm zamiast system w hello world

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Systenm.out.println("Hello, World!");
+        System.out.println("Hello, World!");
         System.out.println("Welcome to our AI AUTOMATION application!");
         Button printButton = new Button("Print Dollar Signs");
         printButton.setOnAction(e -> {
